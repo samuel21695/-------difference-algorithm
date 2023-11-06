d0d79705db0a47d4df5cea9674abeaf416d5e9cf
kongukjae <kongukjae@gmail.com>
2023-11-06 14:07:59 +0900

    controller 파트 부분

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+// built-in modules
+import fs from "fs";
+
+// custom modules
 import diffLogic from "./model/diff-logic.js";
-diffLogic();
+
+// json files path
+const inputJSONPath = "./config/fromDB-data.json";
+const outputJSONPath = "./config/differences.json";
+
+// controller
+
+const resultObject = diffLogic(inputJSONPath, outputJSONPath);
+console.log(resultObject);
