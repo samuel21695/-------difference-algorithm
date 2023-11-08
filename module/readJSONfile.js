2d20d26c77867815e6002fe4e8726cc87de13240
samuel21695 <samuel21695@gmail.com>
2023-11-08 23:13:25 +0900

    ✨ feat: function readJSONfile

--- module/readJSONfile.js
+++ module/readJSONfile.js
@@ -1,3 +1,10 @@
 export default function (filename) {
-  
+  // todo need more research dealing with try catch statement && ESM modules
+  try {
+    const data = fs.readFileSync(filename, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error('Error reading files')
+    return null;
+  }
 }
