be78fb3027114ac3ec518531bc96fd4c11580cf8
samuel21695 <samuel21695@gmail.com>
2023-11-08 22:46:34 +0900

    🧪 test

---
+++ compareFiles.js
@@ -0,0 +1,19 @@
+const fs =require('fs');
+const path = require('path');
+
+// 파일이 .txt 파일인지 확인하는 함수
+const isTxtFile = (fileName) => {
+  // 경로의 확장자 반환
+  // todo .extname .to LowerCase 더 알아보기
+  let result = path.extname(fileName).toLowerCase() === '.txt';
+  return result; // 문자열
+};
+
+// 두 .txt 파일의 내용을 비교하는 함수
+const compareFiles = (operator, operand) => {
+  try { // 예외처리
+  // 파일 확장자 검사
+  } catch {
+    
+  }
+}
--- model/diff-logic.js
+++ model/diff-logic.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 
 /**
  * 
@@ -6,6 +7,7 @@
  * @returns Object
  */
 
+// ESM 방식
 export default function(inputJSONPath, outputJSONPath) {
   if (!inputJSONPath.endsWith('.json') || !outputJSONPath.endsWith('.json')) {
     throw new Error(`매개변수 ${inputJSONPath}, ${outputJSONPath}는 json 파일이 아닙니다.`);
@@ -18,7 +20,7 @@ export default function(inputJSONPath, outputJSONPath) {
    * * 1. inputJSONdata, outputJSONdata를 읽어서 JSON 객체로 변환
    * * 2. inputJSONdata, outputJSONdata의 value를 비교
    * * 3. outputJSONpath 매개변수의 key에 해당하는 정보를 저장
-   * * 4. dirrences.json 파일에 필요한 상태값
+   * * 4. differences.json 파일에 필요한 상태값
    * * 5. 같은 단어가 무엇인지 저장
    * * 6. 다른 단어가 무엇인지 저장
    * * 7. 리턴을 통해 결과값을 전달
